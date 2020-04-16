67412dd68305bbd0f49127a9af70523bc7220eac
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-04-16 18:56:31 +0600

    fix

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+useNewUrlParser: true;
 //MIDDLEWARE
 app.use(express.json({ extended: false }));
 app.use(cors());
